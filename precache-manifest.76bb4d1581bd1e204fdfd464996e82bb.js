f9a3b62728549d074256c103203bc95635fd8638
sourav2831 <sourav2831@gmail.com>
2020-07-13 18:11:52 +0530

    Updates

---
+++ precache-manifest.76bb4d1581bd1e204fdfd464996e82bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4128321ae0ca91ed07e8764ffc09eb70",
+    "url": "/nested-list/index.html"
+  },
+  {
+    "revision": "50d3a5e5cd44c05119cf",
+    "url": "/nested-list/static/css/main.7d76e701.chunk.css"
+  },
+  {
+    "revision": "1611a9307a58ef597f03",
+    "url": "/nested-list/static/js/2.21e86c3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nested-list/static/js/2.21e86c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d3a5e5cd44c05119cf",
+    "url": "/nested-list/static/js/main.03dc0699.chunk.js"
+  },
+  {
+    "revision": "9d7041eccbff612dc978",
+    "url": "/nested-list/static/js/runtime-main.4ea8f8e9.js"
+  }
+]);
